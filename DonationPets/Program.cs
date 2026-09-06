f3377149df5d5011476d6a219fa1e76ba8802a70
well9625 <wellingtonranniery86@gmail.com>
2026-09-06 04:09:19 -0300

    AdoptionPets

--- DonationPets/Program.cs
+++ DonationPets/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using usuarios;
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        Usuario.CriarUsuario criarUsuario = new Usuario.CriarUsuario("João", "joao@email.com", new DateTime(2008, 10, 01));
+        Usuario usuario = criarUsuario.NovoUsuario;
+
+        Console.WriteLine("Nome: " + usuario.Nome);
+        Console.WriteLine("Email: " + usuario.Email);
+        Console.WriteLine("Data de Nascimento: " + usuario.DataNascimento.ToShortDateString());
+        Console.WriteLine("Data de Cadastro: " + Usuario.dataCadastro.ToString());
+        Console.WriteLine(usuario.Idade);
+    }
+}
--- DonationPets/Usuarios/usuarios.cs
+++ DonationPets/Usuarios/usuarios.cs
@@ -1,23 +1,59 @@
-namespace Usuarios
+namespace usuarios
 {
-    
-public abstract class Usuario
-    {
-        public string nome { get; set; }
-        public string email { get; set; }
-        public string senha { get; set; }
-    }
 
-    public class Doador : Usuario
+    public class Usuario
     {
-        
-    }
 
-     public class Donatario : Usuario
-    {
-        public DateTime idade { get; set; }
-        public string endereco { get; set; }
-        
+        public static DateTime dataCadastro { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
 
+        public int Idade
+        {
+            get
+            {
+                DateTime hoje = DateTime.Today;
+                int idade = hoje.Year - DataNascimento.Year;
+                if (DataNascimento.Date > hoje.AddYears(-idade))
+                {
+                    idade--;
+                }
+                return idade;
+            }
+        }
+
+
+        public class CriarUsuario
+        {
+            public Usuario NovoUsuario { get; set; }
+
+            public CriarUsuario(string nome, string email, DateTime dataNascimento)
+            {
+
+
+                if (dataNascimento > DateTime.Now.AddYears(-18))
+                {
+
+                    throw new ArgumentException("Data de nascimento inválida.");
+
+                }
+
+                if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(email))
+                {
+                    throw new ArgumentException("Nome e email não podem ser nulos ou vazios.");
+                }
+                NovoUsuario = new Usuario()
+                {
+
+                    
+                    Nome = nome,
+                    Email = email,
+                    DataNascimento = dataNascimento
+                };
+                Usuario.dataCadastro = DateTime.Now;
+
+            }
+        }
     }
 }
